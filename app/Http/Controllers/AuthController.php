2df0dd6f09c4160b5bbfec167ef0edea2b9eb8ed
emilio-cristobal <ecris1502@gmail.com>
2023-03-04 13:22:38 -0600

    Login añadido

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Helpers\JsonResponse;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            $user = $request->user();
+            $token = $user->createToken('token-name')->plainTextToken;
+            return JsonResponse::sendResponse(['token' => $token]);
+        }
+
+        return JsonResponse::sendError('Credenciales invalidas', 401);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
@@ -24,6 +25,8 @@ use App\Http\Controllers\UserController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::post('auth/login', [AuthController::class, 'login']);
 
 Route::apiResource('employees', EmployeeController::class);
 Route::apiResource('subsidiaries', SubsidiaryController::class);
